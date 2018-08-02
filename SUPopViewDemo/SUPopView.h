8533f3a00e4fc58a5c50c003b927f3675102f882
Superman <703353105@qq.com>
2018-08-02 20:55:29 +0800

    修改代码

---
+++ SUPopViewDemo/SUPopView.h
@@ -0,0 +1,13 @@
+//
+//  SUPopView.h
+//  SUPopViewDemo
+//
+//  Created by Superman on 2018/8/2.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SUPopView : UIView
+
+@end
---
+++ SUPopViewDemo/SUPopView.m
@@ -0,0 +1,21 @@
+//
+//  SUPopView.m
+//  SUPopViewDemo
+//
+//  Created by Superman on 2018/8/2.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import "SUPopView.h"
+
+@implementation SUPopView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
